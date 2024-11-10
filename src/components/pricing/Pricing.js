0bfa208d58758a1d201774bac006bae2eaaa2795
munibk22 <munib22@gmail.com>
2024-11-10 13:51:18 -0500

    Add paypal donation link

--- src/components/pricing/Pricing.js
+++ src/components/pricing/Pricing.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import './pricing.css'
+import PayPalDonation from '../donate/PayPalDonation';
 // import DonateButton from '../donate/DonateButton';
+
 
 function Pricing() {
     return (<>
-        <article className='card-article'>
-            <div className=''>
+        <article className='card-article'>            
                 <h4>Paypal, Zelle, and Venmo:</h4>
                 {/* <DonateButton />  */}
                 <div id="donate-button-container">
-                    <div id="donate-button"></div>
-                    <img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" alt="UTF-8" />
+                    
+                    {/* <img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" alt="UTF-8" /> */}
+                    <PayPalDonation />
 
                 </div>
-
-            </div>
         </article>
     </>
     );
